2d283c04d11604acb13cef8568e229605a592854
Will Kimeria <wkimeria@gmail.com>
2013-05-12 22:37:11 -0400

    Added Node class that builds tree with word combinations

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,51 @@
+import com.wordpath.utilities.Node
 class BootStrap {
 
     def init = { servletContext ->
+		
+		//For now, just a test of generating 4x3 word puzzles
+		//TODO:Select database, create Puzzle Domain Objects and populate database. The code below should run
+		//     as a background process
+		int wordLength = 4;
+		int puzzleLength = 3
+		List<String> allowedWords = new ArrayList<>();
+		getWordsOfLength(wordLength, allowedWords);
+		System.out.println("Words = " + allowedWords.size());
+		Double randomWordIndex = Math.random() * allowedWords.size();
+		//String randomWord = allowedWords.get(randomWordIndex.intValue());
+		
+		for(String s:allowedWords){
+			String randomWord = s;
+			Node node = new Node(null, randomWord, allowedWords, 0, puzzleLength);
+			println("--------------- Node built for ${s} ---------------------");
+			List<List<String>> paths = node.getAllPaths();
+			for (List<String> path : paths) {
+				println(path);
+			}
+			System.out.println("Possible Paths = ${paths.size()}");
+		}
     }
     def destroy = {
     }
+	
+	final static String FILE_NAME = "data/wordList.txt";
+	
+	def getWordsOfLength(int length, List<String> allowedWords)
+			throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(FILE_NAME));
+		try {
+			String line = br.readLine();
+			while (line != null) {
+				if (line.trim().length() == length) {
+					allowedWords.add(line.trim().toLowerCase());
+				}
+				line = br.readLine();
+			}
+		} catch (Exception e) {
+
+		} finally {
+			br.close();
+		}
+
+	}
 }
--- grails-app/controllers/com/wordPath/PuzzleController.groovy
+++ grails-app/controllers/com/wordPath/PuzzleController.groovy
@@ -7,25 +7,7 @@ import org.codehaus.groovy.grails.web.json.JSONObject
 class PuzzleController {
 
 	def generate(){
-		//For now, hardcode this
-		def path1 = []
-		path1.add("DOE")
-		path1.add("WOE")
-		path1.add("WON")
-		path1.add("WIN")
 		
-		def path2 = []
-		path2.add("DOE")
-		path2.add("SOE")
-		path2.add("SIE")
-		path2.add("WIN")
-		
-		def puzzles =[]
-		puzzles.add(path1)
-		puzzles.add(path2)
-		def resp = new JSONArray(puzzles)
-		preventCache(response)
-		render resp as JSON	
 	}
 	
 	private static final String HEADER_PRAGMA = "Pragma";
