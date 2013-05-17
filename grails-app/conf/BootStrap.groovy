927da6381c95523dcca14995b219cfc43491d46c
Will Kimeria <wkimeria@gmail.com>
2013-05-17 09:31:25 -0400

    Playing around with domain model

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -5,32 +5,34 @@ class BootStrap {
     def init = { servletContext ->
 
 		int wordLength = 4;
-		int puzzleLength = 3
+		int puzzleLength = 5
 		List<String> allowedWords = new ArrayList<>();
 		getWordsOfLength(wordLength, allowedWords);
 		System.out.println("Words = " + allowedWords.size());
 		Double randomWordIndex = Math.random() * allowedWords.size();
 		//String randomWord = allowedWords.get(randomWordIndex.intValue());
 		
-		println new Date()
+		println "Started at ${new Date()}"
+		int recordCount = 0
 		
 		for(String s:allowedWords){
 			String randomWord = s;
 			Node node = new Node(null, randomWord, allowedWords, 0, puzzleLength)			
-			List<List<String>> paths = node.getAllPaths()			
-			for (List<String> path : paths) {
-				//println "Saving ${path}"
+			List<List<String>> paths = node.getAllPaths()
+			
+			for (List<String> path : paths) {				
 				String start = path.first()
 				String last = path.last()				
-				def record = new Puzzle(startWord:start, 'path':path, endWord:last, frequency:0, wordLength:path.size())				
-				record.save(flush:true)			
+				def record = new Puzzle(startWord:start, 'path':path, endWord:last, frequency:paths.size(), wordLength:path.size())				
+				record.save(flush:true)		
+				recordCount++
+				println "records = ${recordCount} path = ${path}"
 							
 			}			
 		}
 		def hotels = Puzzle.list()
 		println "Records in database = ${hotels.size()}"
-		println new Date()
-		println "Possible Paths = ${paths.size()}"
+		println "Completed at at ${new Date()}"
     }
     def destroy = {
     }
